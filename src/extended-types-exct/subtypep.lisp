72cdff97886c4a9c3e068489899eafe1290060bb
digikar99 <shubhamkarayare@gmail.com>
2022-05-19 14:38:48 +0530

    [change] extensible-compound-types:subtypep not extended-types

--- src/extended-types-exct/subtypep.lisp
+++ src/extended-types-exct/subtypep.lisp
@@ -1,35 +1,30 @@
 (in-package :polymorphic-functions.extended-types)
 
-(define-compound-type symbol-or-list-subtype-p (symbol-or-list type-specifier)
+(define-compound-type extensible-compound-types:subtypep (symbol-or-list type-specifier)
   "Denotes the set of type specifiers that are a SUBTYPE of TYPE-SPECIFIER"
   (and (or (symbolp symbol-or-list)
            (listp   symbol-or-list))
        (type-specifier-p symbol-or-list)
        (subtypep symbol-or-list type-specifier)))
 
-(deftype extensible-compound-types:subtypep (type-specifier)
-  "Denotes the set of type specifiers that are a SUBTYPE of TYPE-SPECIFIER"
-  `(and (or symbol list)
-        (symbol-or-list-subtype-p ,type-specifier)))
-
-(defmethod %upgraded-cl-type ((name (eql 'symbol-or-list-subtype-p)) type &optional env)
+(defmethod %upgraded-cl-type ((name (eql 'extensible-compound-types:subtypep)) type &optional env)
   (declare (ignore env))
   (etypecase type
     (symbol 'symbol)
     (list   'list)))
 
-(defmethod %subtypep ((n1 (eql 'symbol-or-list-subtype-p))
-                      (n2 (eql 'symbol-or-list-subtype-p))
+(defmethod %subtypep ((n1 (eql 'extensible-compound-types:subtypep))
+                      (n2 (eql 'extensible-compound-types:subtypep))
                       t1 t2 &optional env)
-  (optima.extra:let-match (((list 'symbol-or-list-subtype-p ts1) t1)
-                           ((list 'symbol-or-list-subtype-p ts2) t2))
+  (optima.extra:let-match (((list 'extensible-compound-types:subtypep ts1) t1)
+                           ((list 'extensible-compound-types:subtypep ts2) t2))
     (multiple-value-bind (subtypep knownp) (subtypep ts1 ts2 env)
       (values subtypep knownp))))
 
 (defmethod %subtypep ((n1 (eql 'member))
-                      (n2 (eql 'symbol-or-list-subtype-p))
+                      (n2 (eql 'extensible-compound-types:subtypep))
                       t1 t2 &optional env)
-  (optima.extra:let-match (((list 'symbol-or-list-subtype-p specifier) t2)
+  (optima.extra:let-match (((list 'extensible-compound-types:subtypep specifier) t2)
                            ((list* 'member objects) t1))
     (values (every (lambda (object)
                      (if (type-specifier-p object)
