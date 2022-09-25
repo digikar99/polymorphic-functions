16bcd82d9ddbac56dc03732e324a6d3461eafe8e
digikar99 <shubhamkarayare@gmail.com>
2022-09-25 23:18:33 +0530

    [optim] specializing: dispatch using a single lambda

--- src/specializing.lisp
+++ src/specializing.lisp
@@ -23,22 +23,6 @@
   (declare (ignore object))
   '(signed-byte 32))
 
-(defun find-specialized-lambda (table &rest args)
-  (loop :for (applicable-p-lambda specialized-lambda specialized-type-list) :in table
-        :if (apply applicable-p-lambda args)
-          :do (return-from find-specialized-lambda specialized-lambda)))
-
-(defun specialized-lambda-applicable-p-lambda (&rest arg-values)
-  (let ((args (make-gensym-list (length arg-values))))
-    (compile nil `(lambda (,@args)
-                    (declare (optimize speed)
-                             #+sbcl (sb-ext:muffle-conditions sb-ext:compiler-note)
-                             (compiler-macro-notes:muffle
-                              compiler-macro-notes:optimization-failure-note))
-                    (and ,@(loop :for arg-value :in arg-values
-                                 :for arg :in args
-                                 :collect `(typep ,arg ',(specializing-type-of arg-value))))))))
-
 (defun add-specialization (new-specialization table)
   "NEW-SPECIALIZATION should be a list of three elements
     SPECIALIZATION-APPLICABLE-P-LAMBDA SPECIALIZED-LAMBDA TYPE-LIST"
@@ -53,7 +37,42 @@
            (list new-specialization)
            (subseq table specialization-position))))
 
-(defvar *specialization-table* (trivial-garbage:make-weak-hash-table))
+(defun add-specialization (new-specialization table &rest arg-values)
+  "NEW-SPECIALIZATION should be a list of two elements
+     TYPE-LIST SPECIALIZED-LAMBDA"
+  (let* ((new-type-list (first new-specialization))
+         (specialization-position
+           (loop :for position :from 0
+                 :for old-specialization :in table
+                 :for old-type-list := (first old-specialization)
+                 :while (type-list-more-specific-p old-type-list new-type-list)
+                 :finally (return position)))
+         (all-specializations
+           (nconc (subseq table 0 specialization-position)
+                  (list new-specialization)
+                  (subseq table specialization-position)))
+         (args (make-gensym-list (length arg-values))))
+    (cons (compile nil
+                   `(lambda (,@args)
+                      (declare (optimize speed)
+                               #+sbcl (sb-ext:muffle-conditions sb-ext:compiler-note)
+                               (compiler-macro-notes:muffle
+                                compiler-macro-notes:optimization-failure-note))
+                      (cond ,@(mapcar (lambda (specialization)
+                                        (destructuring-bind
+                                          (type-list specialized-lambda)
+                                            specialization
+                                          `((and ,@(loop :for type :in type-list
+                                                         :for arg :in args
+                                                         :collect `(typep ,arg ',type)))
+                                            ,specialized-lambda)))
+                                      all-specializations))))
+          all-specializations)))
+
+(defvar *specialization-table* (trivial-garbage:make-weak-hash-table)
+  "A weak-hash-table mapping a global specialization-idx counter to
+a CONS containing the specialization-dispatcher-lambda as the CAR
+and the specializations as the CDR.")
 
 (declaim (type hash-table *specialization-table*))
 (defvar *specialization-count* 0)
@@ -62,26 +81,31 @@
   (assert (every #'symbolp vars))
   (let ((specialization-idx *specialization-count*))
     (incf *specialization-count*)
-    (with-gensyms (specialized-lambda var-types var-type var)
-      `(let* ((,specialized-lambda
-                (or (find-specialized-lambda (gethash ,specialization-idx *specialization-table*) ,@vars)
-                    (let* ((,var-types (mapcar #'specializing-type-of (list ,@vars)))
-                           (,specialized-lambda
-                             (compile nil `(lambda ,',vars
-                                             (declare ,@(loop :for ,var :in ',vars
-                                                              :for ,var-type :in ,var-types
-                                                              :collect (list
-                                                                        'type
-                                                                        ,var-type
-                                                                        ,var)))
-                                             ,@',body))))
-                      (setf (gethash ,specialization-idx *specialization-table*)
-                            (add-specialization
-                             (list
-                              (specialized-lambda-applicable-p-lambda ,@vars)
-                              ,specialized-lambda
-                              (list ,@(loop :For var :in vars
-                                            :collect `(specializing-type-of ,var))))
-                             (gethash ,specialization-idx *specialization-table*)))
-                      ,specialized-lambda))))
-         (funcall ,specialized-lambda ,@vars)))))
+    (with-gensyms (specialized-lambda var-types var-type var add-new-specialization)
+      `(flet ((,add-new-specialization ()
+                (let* ((,var-types (mapcar #'specializing-type-of (list ,@vars)))
+                       (,specialized-lambda
+                         (compile nil `(lambda ,',vars
+                                         (declare (optimize speed))
+                                         (declare ,@(loop :for ,var :in ',vars
+                                                          :for ,var-type :in ,var-types
+                                                          :collect (list
+                                                                    'type
+                                                                    ,var-type
+                                                                    ,var)))
+                                         ,@',body))))
+                  (setf (gethash ,specialization-idx *specialization-table*)
+                        (add-specialization
+                         (list
+                          (list ,@(loop :For var :in vars
+                                        :collect `(specializing-type-of ,var)))
+                          ,specialized-lambda)
+                         (cdr (gethash ,specialization-idx *specialization-table*))
+                         ,@vars))
+                  ,specialized-lambda)))
+         (let* ((,specialized-lambda
+                  (if (gethash ,specialization-idx *specialization-table*)
+                      (or (funcall (car (gethash ,specialization-idx *specialization-table*)) ,@vars)
+                          (,add-new-specialization))
+                      (,add-new-specialization))))
+           (funcall ,specialized-lambda ,@vars))))))
